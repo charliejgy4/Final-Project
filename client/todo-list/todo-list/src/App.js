9f5841eadbc15773b83256951f9300bc2d0ecdd9
Charlie Joseph <charliejgy4@gmail.com>
2022-12-16 17:15:08 +0000

    first commit

--- client/todo-list/todo-list/src/App.js
+++ client/todo-list/todo-list/src/App.js
@@ -1,108 +1,110 @@
+
 import {useState, useEffect} from 'react';
-import axios from 'axios';
+ import axios from 'axios';
 import './App.css';
 
-function App() {
-  const [itemText, setItemText] = useState('');
-  const [listItems, setListItems] = useState([]);
-  const [isUpdating, setIsUpdating] = useState('');
-  const [updateItemText, setUpdateItemText] = useState('');
 
-  //add new todo item to database
-  const addItem = async (e) => {
+
+
+  function App() {
+    const [itemText, setItemText]=useState('');
+    const [listItems, setListItems]=useState([]);
+    const [isUpdating, setIsUpdating]=useState('');
+    const [updateItemText, setUpdateItemText]= useState('');
+
+
+
+    const addItem =async (e)=>{
     e.preventDefault();
     try{
-      const res = await axios.post('http://localhost:3001/api/item', {item: itemText})
-      setListItems(prev => [...prev, res.data]);
-      setItemText('');
-    }catch(err){
-      console.log(err);
+const res= await axios.post ('http://localhost:3000/api/item', {item:itemText})
+setListItems(prev => [...prev,res.data]);
+setItemText('');
+    } catch (err){
+      console.log (err);
     }
-  }
-
-  //Create function to fetch all todo items from database -- we will use useEffect hook
-  useEffect(()=>{
-    const getItemsList = async () => {
-      try{
-        const res = await axios.get('http://localhost:3001/api/items')
-        setListItems(res.data);
-        console.log('render')
-      }catch(err){
-        console.log(err);
-      }
-    }
-    getItemsList()
-  },[]);
-
-  // Delete item when click on delete
-  const deleteItem = async (id) => {
-    try{
-      const res = await axios.delete(`http://localhost:3001/api/item/${id}`)
-      const newListItems = listItems.filter(item=> item._id !== id);
-      setListItems(newListItems);
-    }catch(err){
-      console.log(err);
-    }
-  }
-
-  //Update item
-  const updateItem = async (e) => {
-    e.preventDefault()
-    try{
-      const res = await axios.put(`http://localhost:3001/api/item/${isUpdating}`, {item: updateItemText})
-      console.log(res.data)
-      const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
-      const updatedItem = listItems[updatedItemIndex].item = updateItemText;
-      setUpdateItemText('');
-      setIsUpdating('');
-    }catch(err){
-      console.log(err);
-    }
-  }
-  //before updating item we need to show input field where we will create our updated item
-  const renderUpdateForm = () => (
-    <form className="update-form" onSubmit={(e)=>{updateItem(e)}} >
-      <input className="update-new-input" type="text" placeholder="New Item" onChange={e=>{setUpdateItemText(e.target.value)}} value={updateItemText} />
-      <button className="update-new-btn" type="submit">Update</button>
-    </form>
-  )
-
-  return (
-    <div className="App">
-      <h1>Todo APP</h1>
-      <form className="form" onSubmit={e => addItem(e)}>
-        <input type="text" placeholder='Add Todo Item' onChange={e => {setItemText(e.target.value)} } value={itemText} />
-        <button type="submit">Add</button>
-      </form>
-      <div className="todo-listItems">
-        {
-          listItems.map(item => (
-          <div className="todo-item">
-            {
-              isUpdating === item._id
-              ? renderUpdateForm()
-              : <>
-                  <p className="item-content">{item.item}</p>
-                  <button className="update-item" onClick={()=>{setIsUpdating(item._id)}}>Update</button>
-                  <button className="delete-item" onClick={()=>{deleteItem(item._id)}}>Delete</button>
-                </>
-            }
-          </div>
-          ))
-        }
-        
-
-      </div>
-    </div>
-  );
+  
 }
 
+useEffect(()=>{
+  const getItemList = async()=>{
+    try{
+      const res =await axios.get('http://localhost:3000/api/items')
+    setListItems(res.data);
+    console.log('render')
+
+    }catch(err){
+      console.log(err);
+    }
+  }
+  //getItemsLis()
+
+},[listItems]);
+
+const deleteItem = async (id) => {
+  try{
+const res = await axios.delete(`http://localhost:3000/api/item/${id}`)
+  const newListItems = listItems.filter(item=> item._id !== id);
+  setListItems(newListItems);
+  // setListItems(res.data)
+  // console.log()
+//console.log(res.data);
+}catch (err){
+    console.log(err)
+  }
+}
+
+const updateItem =async (e) =>{
+  e.preventDefault()
+ try{
+const res =await axios.put(`http://localhost:3000/api/item/${isUpdating}`, {item:updateItemText})
+console.log(res.data)
+const updateItemIndex = listItems.findIndex(item => item._id === isUpdating);
+const updateItem = listItems[updateItemIndex].item = updateItemText;
+setUpdateItemText('');
+setIsUpdating('');
+}catch(err){
+  console.log(err);
+ }
+}
+
+const renderUpdateFarm = () => (
+  <form classNameForm = "update-form" onSubmit={(e)=>{updateItem(e)}}>
+     <input className="update-new-input" type = "text" placeholder="New Item" onChange={e=>{setUpdateItemText(e.target.value)}} value={updateItemText} /> 
+    <button className="update-new-btn" type=" submit"> Update</button>
+  </form>
+  )
+  return(
+
+    <div className="App">
+      <h1>To Do App</h1>
+        <form className="form" onSubmit= {e => addItem(e)}>
+          <input type ="text" placeholder ='Add todo Item' onChange={e=>{setItemText(e.target.value)}} value={itemText}/>
+          <button type="submit">Add</button>
+        </form>
+        
+        <div className="todo-listItems">
+          {
+      listItems.map(item =>(
+     <div className="todo-item">
+      {
+        isUpdating  === item._id
+        ? renderUpdateFarm  ()
+        :<>
+        <p className="item-content"> {item.item} </p>
+        <button className="update-item" onClick={() =>{setIsUpdating(item._id)}}>Update</button>
+        <button className="delete-item" onClick={()=> {deleteItem (item._id)}}>Delete</button>
+        </>
+  }   
+</div>
+      ))
+      }
+      </div>
+      </div>
+  );
+    }
+
 export default App;
-
-
-
-
-
 
 
 
@@ -121,7 +123,7 @@ export default App;
 //   const addItem = async (e) => {
 //     e.preventDefault();
 //     try{
-//       const res = await axios.post('http://localhost:3001/api/item', {item: itemText})
+//       const res = await axios.post('http://localhost:3000/api/item', {item: itemText})
 //       setListItems(prev => [...prev, res.data]);
 //       setItemText('');
 //     }catch(err){
@@ -133,7 +135,7 @@ export default App;
 //   useEffect(()=>{
 //     const getItemsList = async () => {
 //       try{
-//         const res = await axios.get('http://localhost:3001/api/items')
+//         const res = await axios.get('http://localhost:3000/api/items')
 //         setListItems(res.data);
 //         console.log('render')
 //       }catch(err){
@@ -141,12 +143,12 @@ export default App;
 //       }
 //     }
 //     getItemsList()
-//   },[]);
+//   },[listItems]);
 
 //   // Delete item when click on delete
 //   const deleteItem = async (id) => {
 //     try{
-//       const res= await axios.delete(`http://localhost:3001/api/item/${id}`)
+//       const res = await axios.delete(`http://localhost:3000/api/item/${id}`)
 //       const newListItems = listItems.filter(item=> item._id !== id);
 //       setListItems(newListItems);
 //     }catch(err){
@@ -158,7 +160,7 @@ export default App;
 //   const updateItem = async (e) => {
 //     e.preventDefault()
 //     try{
-//       const res = await axios.put(`http://localhost:3001/api/item/${isUpdating}`, {item: updateItemText})
+//       const res = await axios.put(`http://localhost:3000/api/item/${isUpdating}`, {item: updateItemText})
 //       console.log(res.data)
 //       const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
 //       const updatedItem = listItems[updatedItemIndex].item = updateItemText;
@@ -178,7 +180,7 @@ export default App;
 
 //   return (
 //     <div className="App">
-//       <h1>ToDo APP</h1>
+//       <h1>Todo List</h1>
 //       <form className="form" onSubmit={e => addItem(e)}>
 //         <input type="text" placeholder='Add Todo Item' onChange={e => {setItemText(e.target.value)} } value={itemText} />
 //         <button type="submit">Add</button>
@@ -207,4 +209,3 @@ export default App;
 // }
 
 // export default App;
-
--- server/index.js
+++ server/index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const connection = require ('./db')
+const userRoutes = require("./routes/users");
+const authRoutes =require ("./routes/auth");
 
 const app = express();
 //use express.json() to get data into json format
 app.use(express.json());
+
+
+//database connection
+//connection();
 //Port 
 const PORT = process.env.PORT || 3000;
 
 //use cors
 app.use(cors());
+app.use("/api/users", userRoutes);
+app.use("/api/auth",authRoutes);
 
 // //import routes
 const TodoItemRoute = require('./routes/todoItems');
@@ -29,3 +38,97 @@ app.use('/', TodoItemRoute);
 
 //server
 app.listen(PORT, ()=> console.log("Server connected") );
+
+
+ //login section starts here
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+//  }
+  
+// const express = require('express')
+// const app = express()
+
+
+// const bcrypt = require('bcrypt')
+// const passport = require('passport')
+// const flash = require('express-flash')
+// const session = require('express-session')
+// const methodOverride = require('method-override')
+  
+//   const initializePassport = require('./passport-config')
+//   initializePassport(
+//     passport,
+//     email => users.find(user => user.email === email),
+//     id => users.find(user => user.id === id)
+//   )
+  
+//  const users = []
+  
+// app.set('view-engine', 'ejs')
+//  app.use(express.urlencoded({ extended: false }))
+// app.use(flash())
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+//   }))
+// app.use(passport.initialize())
+// app.use(passport.session())
+//  app.use(methodOverride('_method'))
+  
+//   app.get('/', checkAuthenticated, (req, res) => {
+//     res.render('index.ejs', { name: req.user.name })
+//   })
+  
+//   app.get('/login', checkNotAuthenticated, (req, res) => {
+//     res.render('login.ejs')
+//   })
+  
+//   app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureRedirect: '/login',
+//     failureFlash: true
+//   }))
+  
+//   app.get('/register', checkNotAuthenticated, (req, res) => {
+//     res.render('register.ejs')
+//   })
+  
+//   app.post('/register', checkNotAuthenticated, async (req, res) => {
+//     try {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//       users.push({
+//         id: Date.now().toString(),
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: hashedPassword
+//       })
+//       res.redirect('/login')
+//     } catch {
+//       res.redirect('/register')
+//     }
+//   })
+  
+//   app.delete('/logout', (req, res) => {
+//     req.logOut()
+//     res.redirect('/login')
+//   })
+  
+//   function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return next()
+//     }
+  
+//     res.redirect('/login')
+//   }
+  
+//   function checkNotAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return res.redirect('/')
+//     }
+//     next()
+//   }
+  
+//   app.listen(3000)
+
+//app.listen(PORT, ()=> console.log("Server connected") );
